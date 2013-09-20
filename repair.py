7dca269567ac674c97646808d748165e9cc54702
lnitram <MartinLeuschner@googlemail.com>
2013-09-20 13:50:26 +0200

    remove hardcoded paths and filenames. Use command line arguments instead

--- repair.py
+++ repair.py
@@ -1,12 +1,21 @@
+import sys
 import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
 
-schema = avro.schema.parse(open("/path/to/schema.json").read())
+if len(sys.argv) != 4:
+    print "Usage: python repair.py /path/to/schema.json brokenfile.avro repairedfile.avro"
+    sys.exit(0)
+    
+schemafile = sys.argv[1]
+infile=sys.arv[2]
+outfile=sys.argv[3]
 
-writer = DataFileWriter(open("repaired.avro", "w"), DatumWriter(), schema)
+schema = avro.schema.parse(open(schemafile).read())
 
-reader = DataFileReader(open("broken.avro", "r"), DatumReader())
+writer = DataFileWriter(open(outfile, "w"), DatumWriter(), schema)
+
+reader = DataFileReader(open(infile, "r"), DatumReader())
 for row in reader:
     writer.append(row)
 reader.close()
